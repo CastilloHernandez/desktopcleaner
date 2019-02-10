d2a178fcfc8c7e55870f7c6742868ea2b4cc22e5
Emmanuel Castillo <emmanuel156@gmail.com>
2019-02-10 17:20:18 -0600

    Se agregan mas opciones de linea de comandos

--- Programas/desktopcleaner.py
+++ Programas/desktopcleaner.py
@@ -3,30 +3,35 @@ import os
 import hashlib
 import time
 import shutil
+import argparse
+import fnmatch
+import re
+import itertools
+import logging
 
 def hashArchivo(fname):
-	hash_md5 = hashlib.md5()
-	try:
-		with open(fname, "rb") as f:
-			for chunk in iter(lambda: f.read(4096), b""):
-				hash_md5.update(chunk)
-	except:
-		print 'error accediendo al archivo ' + fname
-	return hash_md5.hexdigest()
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 def removeIfEmpty(dir, raiz=0):
 	for entry in os.listdir(dir):
-		#print "entry "+ os.path.join(dir, entry) +" "+ str(os.path.isdir(os.path.join(dir, entry)))
 		if os.path.isdir(os.path.join(dir, entry)):
 			removeIfEmpty(os.path.join(dir, entry))
 	if raiz == 0:
 		if not os.listdir(dir):
 			print 'borrando directorio vacio '+ dir
-			os.rmdir(dir)		
-
+			try:
+				os.rmdir(dir)		
+			except:
+				print 'error al borrar directorio '+ dir
 
 def crearDirectorios(ruta):
-	if not os.path.isdir(ruta):
+	if os.path.isdir(ruta):
+		print 'el directorio ya existe '+ ruta 
+	else:
 		print 'crear directorio '+ ruta 
 		os.makedirs(ruta)
 
@@ -41,68 +46,109 @@ def removeLost():
 	db.commit()
 	
 	
-def buscarArchivos(dir):
+def buscarArchivos(dir, destino):
 	global db
 	cursor = db.cursor()
+	if opt.exclude:
+		excludes = list(itertools.chain(*opt.exclude))	
+		rexcludes = r'|'.join([fnmatch.translate(x) for x in excludes]) or r'$.'
+		
 	for root, dirs, files in os.walk(dir):
 		print 'buscando en '+ root
-		#if not os.path.normpath(root) == os.path.normpath(dir):
-		#	removeIfEmpty(root)
+		if opt.exclude:
+			dirs[:] = [d for d in dirs if not re.match(rexcludes, d)]
+			files[:] = [f for f in files if not re.match(rexcludes, f)]
+		
 		for file in files:
 			archivoEncontrado = 0
 			archivoModificado = 0
 			fechaModificado = 0
 			idActual = 0
 			rutaActual = str(os.path.join(root, file))
-			if file == 'desktop.ini':
-				print 'archivo omitido ' + rutaActual
-			else:
-				try:
-					tamanioActual = os.path.getsize(rutaActual)
-				except:
-					print 'error al acceder ' + rutaActual
-					continue
+			try:
 				hashActual = hashArchivo(rutaActual)
-				cursor.execute('SELECT id, path, hash, lastdiff FROM files WHERE path="'+rutaActual+'"')
-				for row in cursor.fetchall():
-					archivoEncontrado = 1
-					idActual = row[0]
-					fechaModificado = row[3]
-					if not row[2] == hashActual:
-						archivoModificado = 1
-				if archivoEncontrado:
-					if archivoModificado:
-						print 'archivo modificado '+ rutaActual
-						cursor.execute('UPDATE files SET size=?, hash=?, lastcheck=?, lastdiff=? WHERE id=?', (tamanioActual, hashActual, time.time(), time.time(), idActual))
-						db.commit()
-					else:
-						if time.time() - fechaModificado > 604800:
-							print 'mover archivo '+ rutaActual
-							dirDestino = os.path.normpath(str(root).replace(dir, 'C:\\Temp\\'))
-							crearDirectorios(dirDestino)
+			except:
+				continue
+				
+			tamanioActual = os.path.getsize(rutaActual)
+			cursor.execute('SELECT id, path, hash, lastdiff FROM files WHERE path="'+rutaActual+'"')
+			for row in cursor.fetchall():
+				archivoEncontrado = 1
+				idActual = row[0]
+				fechaModificado = row[3]
+				if not row[2] == hashActual:
+					archivoModificado = 1
+			if archivoEncontrado:
+				if archivoModificado:
+					print 'archivo modificado '+ rutaActual
+					cursor.execute('UPDATE files SET size=?, hash=?, lastcheck=?, lastdiff=? WHERE id=?', (tamanioActual, hashActual, time.time(), time.time(), idActual))
+					db.commit()
+				else:
+					if time.time() - fechaModificado > 604800:
+						print 'mover archivo '+ rutaActual
+						dirDestino = os.path.normpath(str(root).replace(dir, destino))
+						crearDirectorios(dirDestino)
+						try:
 							shutil.move(rutaActual, os.path.join(dirDestino, file))
+						except:
+							print 'error al mover archivo '+ rutaActual
+							logger.error('error al mover el archivo '+ rutaActual)
+						else:
 							if os.path.isfile(os.path.join(dirDestino, file)):
-								#if not os.path.normpath(root) == os.path.normpath(dir):
-								#	removeIfEmpty(root)
+								logger.info('archivo movido '+ rutaActual +' a '+ dirDestino)
 								cursor.execute('DELETE FROM files WHERE id='+ str(idActual))
 								db.commit()		
-						else:
-							cursor.execute('UPDATE files SET lastcheck=? WHERE id=?', (time.time(), idActual))
-							db.commit()
-				else:
-					print 'nuevo archivo '+ rutaActual
-					cursor.execute('INSERT INTO files(path, name, size, hash, lastcheck, lastdiff) VALUES(?,?,?,?,?,?)', (rutaActual, file, tamanioActual, hashActual, time.time(), time.time() ))
-					db.commit()
+					else:
+						cursor.execute('UPDATE files SET lastcheck=? WHERE id=?', (time.time(), idActual))
+						db.commit()
+			else:
+				print 'nuevo archivo '+ rutaActual
+				cursor.execute('INSERT INTO files(path, name, size, hash, lastcheck, lastdiff) VALUES(?,?,?,?,?,?)', (rutaActual, file, tamanioActual, hashActual, time.time(), time.time() ))
+				db.commit()
 	removeIfEmpty(dir, 1)
-					
+
+parser = argparse.ArgumentParser(prog='pybackup')
+parser.add_argument('-o', action="append", required=True)
+parser.add_argument('-d', action="append", required=True)
+parser.add_argument('-maxsize', default='100M')
+parser.add_argument('-n', type=int, default=3)
+parser.add_argument('-r', default='1M')
+parser.add_argument('-exclude', action="append", nargs='*')
+
+opt = parser.parse_args()				
+
+	
+	
 db = sqlite3.connect('desktopcleaner.db')
 db.text_factory = lambda x: unicode(x, "utf-8", "ignore")
 
 cursor = db.cursor()
 cursor.execute('CREATE TABLE IF NOT EXISTS files (id INTEGER PRIMARY KEY, path TEXT, name TEXT, size INTEGER, hash TEXT, lastcheck INTEGER, lastdiff INTEGER)')
 db.commit()
-buscarArchivos('C:\\Users\\Emmanuel Castillo\\Documents\\Bluetooth Folder\\')
-buscarArchivos('C:\\Users\\Emmanuel Castillo\\Dropbox\\Capturas de pantalla\\')
-buscarArchivos('C:\\Users\\Emmanuel Castillo\\Desktop\\')
+
+
+for logdir in opt.d:
+	if not logdir.endswith(os.path.sep):
+		logdir = logdir + os.path.sep 
+	if not (os.path.isdir(logdir)):
+		os.makedirs(logdir)
+
+logger = logging.getLogger('desktopcleaner')
+hdlr = logging.FileHandler(logdir + 'desktopcleaner.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
+
+
+for destino in opt.d:
+	for origen in opt.o:
+		if not origen.endswith(os.path.sep):
+			origen = origen + os.path.sep
+		if not destino.endswith(os.path.sep):
+			destino = destino + os.path.sep		
+		buscarArchivos(origen, destino)
+		
+
 removeLost()
 db.close()
